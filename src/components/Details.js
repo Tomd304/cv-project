ca77fb1d745da6c6767bc69ad248d935e1be1865
Tomd304 <tomd304@live.co.uk>
2022-01-19 12:49:47 +0000

    Handlers to submit form info and dynamically switch view to submitted info added

--- src/components/Details.js
+++ src/components/Details.js
@@ -3,30 +3,88 @@ import { Component } from "react";
 class Details extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      editMode: true,
+      firstName: "",
+      lastName: "",
+      email: "",
+      phoneNumber: "",
+    };
+    this.submitClick = this.submitClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
+
+  submitClick() {
+    this.setState({
+      editMode: false,
+    });
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
+
   render() {
-    return (
-      <form className="input-form">
-        <h2 className="form-header">Personal Details</h2>
-        <div className="label-group">
-          <label>First Name</label>
-          <input placeholder="John" />
+    if (this.state.editMode) {
+      return (
+        <div className="component-form">
+          <form className="input-form">
+            <h2 className="form-header">Personal Details</h2>
+            <div className="label-group">
+              <label>First Name</label>
+              <input
+                name="firstName"
+                placeholder="John"
+                value={this.state.firstName}
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="label-group">
+              <label>Last Name</label>
+              <input
+                name="lastName"
+                placeholder="Doe"
+                value={this.state.lastName}
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="label-group">
+              <label>Email</label>
+              <input
+                name="email"
+                placeholder="john@gmail.com"
+                value={this.state.email}
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="label-group">
+              <label>Phone Number</label>
+              <input
+                name="phoneNumber"
+                placeholder="+44 7846589523"
+                value={this.state.phoneNumber}
+                onChange={this.handleChange}
+              />
+            </div>
+            <button type="button" onClick={this.submitClick}>
+              Submit
+            </button>
+          </form>
         </div>
-        <div className="label-group">
-          <label>Last Name</label>
-          <input placeholder="Doe" />
+      );
+    } else {
+      return (
+        <div className="component-form">
+          <h1>{this.state.firstName}</h1>
+          <h1>{this.state.lastName}</h1>
+          <h1>{this.state.email}</h1>
+          <h1>{this.state.phoneNumber}</h1>
         </div>
-        <div className="label-group">
-          <label>Email</label>
-          <input placeholder="john@gmail.com" />
-        </div>
-        <div className="label-group">
-          <label>Phone Number</label>
-          <input placeholder="+44 7846589523" />
-        </div>
-        <button type="button">Submit</button>
-      </form>
-    );
+      );
+    }
   }
 }
 
